fcf1b79acb178e3dd3fa52a6414064f4a472255a
Piyush Chamoli <piyushchamoli11@gmail.com>
2021-05-26 23:35:39 +0530

    done

--- block-BJaabl/code/index.js
+++ block-BJaabl/code/index.js
@@ -51,8 +51,10 @@ calc(20, 10, 'mul'); // 200
 
 
 function calc(numA, numB, operation) {
-  if (typeof(numA) == "number" && typeof(numB) == "number") {
-    if (operation == "add" || operation == "sub" || operation == "mul" || operation == "div") {
+  if (typeof(numA) == "number" || typeof(numB) == "number") {
+    if (operation != "add" || operation != "sub" || operation != "mul" || operation != "div") {
+      alert("Enter Valid Input");
+    } else {
       if (operation == "add") {
         alert(numA+numB);
       } else if (operation == "sub") {
@@ -62,13 +64,36 @@ function calc(numA, numB, operation) {
       } else if (operation == "div") {
         alert(numA/numB);
       }
-    } else {
-      alert("Enter Valid Input");
     }
   } else {
     alert("Enter Valid Input");
   }
 }
+
+// function calc(numA, numB, operation) {
+//   if (typeof(numA) !== "number" || typeof(numB) !== "number") {
+//     alert("Enter Valid Input");
+//   } else {
+//     switch (operation) {
+//       case "add":
+//         return numA + numB;
+//         break;
+//       case "sub":
+//         return numA - numB;
+//         break;
+//       case "mul":
+//         return numA * numB;
+//         break;
+//       case "div":
+//         return numA / numB;
+//         break;
+//       default:
+//         return("Enter valid operation!")
+//     }
+//   }
+// }
+
+
 
 /*
 5. Create a function named `isLeapYear` that accepts a number data type and return `true` or `false` based
@@ -77,7 +102,11 @@ on if the year id leap year or not.
 isLeapYear(2000); // true
 isLeapYear(2001); // false
 */
-
+function isLeapYear(year) {
+  if (year % 400 === 0) return true;
+  if (year % 100 === 0) return false;
+  return year % 4 === 0;
+}
 
 /*
 6. Create a function named `getFactorial` that accepts a number and return the factorial of the number.
