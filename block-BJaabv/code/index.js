5d06f8c500cc9c79c6d65034c71e351122fa02f6
Piyush Chamoli <piyushchamoli11@gmail.com>
2021-05-28 00:08:08 +0530

    update

--- block-BJaabv/code/index.js
+++ block-BJaabv/code/index.js
@@ -8,8 +8,8 @@
   * [ ] Add an additional argument to the function that takes the conversion rate of human to dog years.
 */
 
-function calculateDogAge() {
-  // Your code goes here
+function calculateDogAge(age,rate) {
+  return `Dog's age is ${age*7}`;
 }
 
 /*
@@ -19,8 +19,8 @@ function calculateDogAge() {
   * [ ] Return the total number of movie you will watch.
 */
 
-function calculateMoviesToWatch() {
-  // Your code goes here
+function calculateMoviesToWatch(age,no) {
+  return (90-age)*(no*32);
 }
 
 /*
@@ -29,8 +29,8 @@ function calculateMoviesToWatch() {
   * [ ] Convert it to fahrenheit and return "NN°F is NN°C"
 */
 
-function celsiusToFahrenheit() {
-  // Your code goes here
+function celsiusToFahrenheit(cel) {
+  return `${(cel*9/5)+32}F is ${cel}C`;
 }
 
 /*
@@ -39,8 +39,8 @@ function celsiusToFahrenheit() {
   * [ ] Convert it to celsius and return "NN°F is NN°C"
 */
 
-function celsiusToFahrenheit() {
-  // Your code goes here
+function celsiusToFahrenheit(fah) {
+  return `${fah}F is ${(fah-32)*5/9}C`;
 }
 
 /*
@@ -51,8 +51,16 @@ function celsiusToFahrenheit() {
   * [ ] If the value of n is below 0 return "The number below 1 is not allowed"
 */
 
-function pow() {
-  // Your code goes here
+function pow(n, x) {
+  let total=1;
+  if (n<0) {
+    return "The number below 1 is not allowed";
+  } else {
+    for (let i=1; i<=x; i++) {
+      total *= n;
+    }
+    return total;
+  }
 }
 
 // Test
@@ -66,8 +74,22 @@ pow(-31, 2); // "The number below 1 is not allowed"
 and return sum or product of 1,…,n. If user enters any other value than `sum` or `product` alert `Not a valid Input`.
 */
 
-function sumOrProductOfN() {
-  // Your code goes here
+function sumOrProductOfN(n,str) {
+  if (str != "sum" || str != "product") {
+    alert(`Not a valid Input`);
+  } else if (str == "sum") {
+    let sumTotal=0;
+    for (let i=n; i>=1; i--) {
+      sumTotal += i;
+    }
+    return sumTotal;
+  } else {
+    let mulTotal=1;
+    for (let i=n; i>=1; i--) {
+      mulTotal *= i;
+    }
+    return mulTotal;
+  }
 }
 
 sumOrProductOfN(4, 'sum'); // 10
@@ -78,8 +100,12 @@ sumOrProductOfN(4, 'hello'); // alert "Not a valid Input"
 6. 🎖Write a program that accepts a number n using argument and return the sum of the numbers 1 to n
 */
 
-function sumOfN() {
-  // Your code goes here
+function sumOfN(n) {
+  let total = 0;
+  for(let i=n; i>=1; i--) {
+    total += i;
+  }
+  return total;
 }
 
 /*
